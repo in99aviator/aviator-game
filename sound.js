a0cbbad37eb97feb8fb9bacba07dd03c941ea5f9
in99aviator <in99soft.dev.pvt.ltd@gmail.com>
2025-04-09 22:19:31 +0530

    Update and rename scrip.js to script.js

--- sound.js
+++
@@ -1,7 +0,0 @@
-const crashSound = document.getElementById("crashSound");
-const cashoutSound = document.getElementById("cashoutSound");
-const startSound = document.getElementById("startSound");
-
-function playCrashSound() { crashSound.play(); }
-function playCashoutSound() { cashoutSound.play(); }
-function playStartSound() { startSound.play(); }
